038c1647bb623d5cb2f5cc38ab4375f835c829e6
John Lindner <jlogin56@gmail.com>
2016-05-02 21:10:21 -0400

    First half of mini course

---
+++ ruby/mini-course/first.rb
@@ -0,0 +1,18 @@
+#puts "Hi there!"
+#puts "Welcome to my program!"
+#name = "Jennifer"
+#p name
+#name = "Jen"
+#puts "Hi #{name}!"
+
+#puts "What's your name?"
+#name = gets.chomp
+#puts "Hi #{name}"
+
+puts "Give me a word"
+word_1 = gets.chomp
+puts "Give me a word"
+word_2 = gets.chomp
+
+result = word_1 + word_2
+puts "The result is #{result}"
---
+++ ruby/mini-course/second.rb
@@ -0,0 +1,10 @@
+puts "Do you want something sweet or healthy?"
+answer = gets.chomp
+
+if answer == "sweet"
+	puts "Eat icecream!"
+elsif answer == "healthy"
+	puts "Eat carrots!"
+else 
+	puts "I did not understand you."
+end	
