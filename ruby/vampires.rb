672584f90a6b21293cf27b66e8ed0169dea9c879
John Lindner <jlogin56@gmail.com>
2016-05-03 17:37:29 -0400

    Creating vampire application

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+puts "What is your name?"
+v_name = gets.chomp
+puts "How old are you?"
+v_age = gets.to_i
+puts "What year where you born?"
+v_year = gets.to_i
+puts "Would you like some garlic bread?(y/n)"
+eats_garlic = gets.chomp
+puts "Would you like to enroll in the company's health insurance?(y/n)"
+health_ins = gets.chomp
