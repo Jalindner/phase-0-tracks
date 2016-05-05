1ee736d50096aba9d36f90c5c640a6e32f9bfdee
John Lindner <jlogin56@gmail.com>
2016-05-05 16:33:41 -0400

    Adding decrypt method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -13,4 +13,28 @@ def encrypt(string)
   p encrypt_string
 end
 
-encrypt("today the water fell in teh wrong direction")
+def decrypt(string)
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+  index = 0
+  alpha_index = 0
+  decrypt_string = string
+  while index < string.length
+    if string[index] == " "
+      decrypt_string[index] = string[index]
+      index += 1
+    else
+      until string[index] == alphabet[alpha_index]
+       alpha_index += 1
+      end
+      decrypt_string[index] = alphabet[alpha_index - 1]
+      #Forgot to make the index add up here which 
+      #was causing the infinate loop
+      index += 1
+    end
+    alpha_index = 0
+  end
+  p decrypt_string
+end
+
+encrypted_pass = encrypt("abc")
+decrypted_pass = decrypt(encrypted_pass)
