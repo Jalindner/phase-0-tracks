88adb058a3f2dd985c923a076590e747ff324b99
John Lindner <jlogin56@gmail.com>
2016-05-10 14:18:15 -0400

    Adding methods

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,3 +1,11 @@
+def build_array(first, second, third)
+  display_array = [first, second, third]
+end
+
+def add_to_array(arr, new_element)
+  new_array = arr << new_element
+end
+
 presidental_canidates = []
 p presidental_canidates
 presidental_canidates = ["Sanders", "Cruz", "Trump", "Clinton", "ME!"]
@@ -12,3 +20,6 @@ p presidental_canidates
 celeberties = ["Timberlake", "Cardashian", "Gomez", "Skrillix"]
 world_leaders = presidental_canidates + celeberties
 p world_leaders
+
+p build_array(1, 2, 3)
+p add_to_array(presidental_canidates, "Rubio")
