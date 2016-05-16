2269514e72c4e09586f24454c5e4e0bcca59f62f
John Lindner <jlogin56@gmail.com>
2016-05-16 01:40:16 -0400

    Added validation

--- John_Lindner_Wk5/calculator.rb
+++ John_Lindner_Wk5/calculator.rb
@@ -1,4 +1,5 @@
-#I have struggled to find ways for this to accept
+#I have struggled to find ways for this to accept 
+#multi-digit integers
 def calculate(int_a, int_b, operator)
   result = int_a.public_send operator, int_b
 end
@@ -11,17 +12,23 @@ end
 operation_hash = {}
 operation = ""
 calc_count = 0
-puts "Enter your operation, please do not use spaces, please use single digit integers"
+puts "Enter your operation, please do not use spaces, please stick to single digit integers"
 puts "Enter 'done' when finished"
 until operation == "done"
   operation = gets.chomp
   if operation == "done"
     break
   else
-    answer = calculate(operation[0].to_i, operation[2].to_i, operation[1])
-    puts answer
-    operation_hash[operation] = answer
-    calc_count += 1
+    if operation[0].to_i.to_s != operation[0] || operation[2].to_i.to_s != operation[2]
+      puts "Invalid integers"
+    elsif operation[1] != "+" && operation[1] != "-" && operation[1] != "*" && operation[1] != "/"
+      puts "Invalid operation or double+ digits entered"
+    else
+      answer = calculate(operation[0].to_i, operation[2].to_i, operation[1])
+      puts answer
+      operation_hash[operation] = answer
+      calc_count += 1
+    end
   end
 end
 
