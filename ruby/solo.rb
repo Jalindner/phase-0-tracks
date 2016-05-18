32f2c69769b837218de3039fb2bab7c9f5c84ec7
John Lindner <jlogin56@gmail.com>
2016-05-18 18:59:57 -0400

    Adding update_treasure method

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -20,8 +20,10 @@ class Dragon
     example_types = {"classical" => "fire streaks", "mooshoo" => "fire balls", "undead" => "miasma", "rainbow" => "cheap electronica loops", "deep-sea" => "nothing"}
       if example_types.include?(@type)
         puts "The #{@type} dragon exhales #{example_types[@type]}!"
+        puts " "
       else
         puts "The #{type} dragon is not in our database, proceed with caution!"
+        puts " "
       end
   end
   #update_eggs
@@ -31,7 +33,19 @@ class Dragon
   def update_eggs(new_amt)
     @egg_amt = new_amt
     puts "This dragon now has #{@egg_amt} of eggs"
+    puts " "
   end
+
+  #update_treasure
+    #takes a string as an arguement
+    #favorite_treasure = string arguement
+    #print updated favorite_treasure
+  def update_treasure(new_treasure)
+    @favorite_treasure = new_treasure
+    puts "This dragon's new favorite treasure is #{@favorite_treasure}"
+    puts " "
+  end
+
   #print_dragon
     #prints out all current data about selected dragon
   def print_dragon
@@ -46,7 +60,8 @@ naoki = Dragon.new("mooshoo", "jade structures")
 naoki.breathe
 naoki.update_eggs(2)
 naoki.print_dragon
-
+naoki.update_treasure("gold coins")
+naoki.print_dragon
 
 
 
