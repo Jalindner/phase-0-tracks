8b8cf72d4040c47c09d45d7240bb7ca25ccba1c6
John Lindner <jlogin56@gmail.com>
2016-05-18 20:42:09 -0400

    Finishing user interface

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -8,7 +8,7 @@ class Dragon
     @type = type
     @favorite_treasure = favorite_treasure
     @egg_amt = 0
-    print_dragon
+    #print_dragon
   end
   #breathe
     #what is exhaled depends on the dragon's type
@@ -56,12 +56,86 @@ class Dragon
   end
 end
 
-naoki = Dragon.new("mooshoo", "jade structures")
-naoki.breathe
-naoki.update_eggs(2)
-naoki.print_dragon
-naoki.update_treasure("gold coins")
-naoki.print_dragon
+#Initial driver code
+#naoki = Dragon.new("mooshoo", "jade structures")
+#naoki.breathe
+#naoki.update_eggs(2)
+#naoki.print_dragon
+#naoki.update_treasure("gold coins")
+#naoki.print_dragon
+
+dragons = []
+choice = " "
+puts "Thank you for using our dragon reporting system!"
+until choice == "e"
+  puts "-Type 'n' to create a new dragon or 'e' to exit and finish your report."
+  choice = gets.chomp
+  if choice == "n"
+    puts "Enter the dragon's type"
+    type = gets.chomp
+    puts "Enter the dragon's favorite treasure"
+    treasure = gets.chomp
+    dragons << Dragon.new(type, treasure)
+  elsif choice == "e"
+    break
+  else
+    puts "Invalid input"
+  end
+end
+
+dragons.length.times do |index|
+  puts "-Dragon number #{index + 1}"
+  dragons[index].print_dragon
+end
+
+dragon_choice = " "
+edit_choice = " "
+puts "Is their further information about the dragon(s) you would like to add?"
+until dragon_choice == "e"
+  puts "-Enter the dragons associated number on the printed list to select that dragon to edit."
+  puts "-Enter 'list' to review the data so far."
+  puts "-Enter 'e' to exit the program and finish your edits"
+  dragon_choice = gets.chomp
+  if dragon_choice == "e"
+    break
+  elsif dragon_choice == "list"
+    dragons.length.times do |index|
+      puts "-Dragon number #{index + 1}"
+      dragons[index].print_dragon
+    end
+  else
+    puts "-Enter 'eggs' to update the amount of eggs the dragon has or 'treasure to update its favorite treasure."
+    puts "-Enter 'breathe' to test the dragon's breath."
+    puts "-Enter 'e' to exit to the previous menu."
+    until edit_choice == "e"
+      edit_choice = gets.chomp
+      if edit_choice == "eggs"
+        puts "Enter the new amount of eggs"
+        new_eggs = gets.to_i
+        dragons[dragon_choice.to_i - 1].update_eggs(new_eggs)
+        break
+      elsif edit_choice == "treasure"
+        puts "Enter the new favorite treasure"
+        new_treasure = gets.chomp
+        dragons[dragon_choice.to_i - 1].update_treasure(new_treasure)
+        break
+      elsif edit_choice == "breathe"
+        dragons[dragon_choice.to_i - 1].breathe
+        break
+      elsif edit_choice == "e"
+        break
+      else
+        puts "Invalid choice"
+      end
+    end
+  end
+end
+
+dragons.length.times do |index|
+  puts "-Dragon number #{index + 1}"
+  dragons[index].print_dragon
+end
+
 
 
 
