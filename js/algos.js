efcab69e38d58a2890779b2bb856958906288694
John Lindner <jlogin56@gmail.com>
2016-05-26 22:59:10 -0400

    Adding EqualPairs method and driver code

--- js/algos.js
+++ js/algos.js
@@ -30,8 +30,48 @@ function LongestElement(stringArray){
   };
 }
 
+
+//Create a function that takes two objects as arguements
+function EqualPairs(objectA, objectB){
+  this.objectA = objectA;
+  this.objectB = objectB;
+  
+  
+  //function that compares all keys and their values
+  //create variable hasEquals = false
+  //for each key in objectA
+    //for each key in objectB
+      //if objectA[key] value == objectB[key] value
+        //hasEquals = true
+  this.tryEqual = function() {
+    hasEquals = false;
+    for (var keyA in objectA){
+      for (var keyB in objectB){
+        if (objectA[keyA] == objectB[keyB]){
+          hasEquals = true;
+          console.log(hasEquals);
+          break;
+        }
+        break;
+      }
+    }
+  };
+}
+
+
 var basicArray = new LongestElement(["longestestester", "long", "longer", "longestest", "longest"]);
 basicArray.stringLength();
 console.log("__________________________");
 var nameArray = new LongestElement(["Tina", "Carol", "Sarah", "Evey"]);
 nameArray.stringLength();
+
+mark = {name: "Mark", age: 23, occupation: "Engineer"};
+angela = {name: "Angela", age: 24, occupation: "biologist", favoritePet: "Dogs"};
+markTheSecond = {name: "Mark", favoritePet: "Dogs", age: 23};
+
+compare = new EqualPairs(mark, angela);
+compare.tryEqual();
+compareSecond = new EqualPairs(mark, markTheSecond);
+compareSecond.tryEqual();
+compareThird = new EqualPairs(angela, markTheSecond);
+compareThird.tryEqual();
