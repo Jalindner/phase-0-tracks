fe84e153021b7520fc2c81bf4cd837914122b560
John Lindner <jlogin56@gmail.com>
2016-05-26 20:39:27 -0400

    Correcting function to print longest elements

--- js/algos.js
+++ js/algos.js
@@ -5,21 +5,33 @@ function LongestElement(stringArray){
   
   //create a function that searches the array for the 
   //element with the longest string
+  this.stringLength = function(){
     //create an integer variable = 0 that stands for the longest element so far
+    var longest = 0;
     //for each element in the array
       //if the elements string.length is greater than the current longest value
-      //longest = stringArray[i].length
-  this.stringLength = function(){
-    var longest = 0;
+        //longest = stringArray[i].length
     for (var i = 0; i < stringArray.length; i ++){
       console.log(stringArray[i].length);
       if (stringArray[i].length > longest){
         longest = stringArray[i].length;
       }
     }
-    console.log("The longest array has " + longest + " characters.");
+    console.log("The longest strings in this array are: ");
+    //match the element or elements with the final longest value
+    //for each element in the array
+      //if the array's element.length == longest
+        //print that element and the length
+    for (var i = 0; i < stringArray.length; i++){
+      if (stringArray[i].length == longest){
+        console.log(stringArray[i] + ": " + longest + " chars");
+      }
+    }
   };
 }
 
-var userArray = new LongestElement(["longestestester", "long", "longer", "longestest", "longest"]);
-userArray.stringLength();
+var basicArray = new LongestElement(["longestestester", "long", "longer", "longestest", "longest"]);
+basicArray.stringLength();
+console.log("__________________________");
+var nameArray = new LongestElement(["Tina", "Carol", "Sarah", "Evey"]);
+nameArray.stringLength();
